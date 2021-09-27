982310f2b41e318504bef7bd21f406838bc302f8
Christian <chibuike4jamb@gmail.com>
2021-09-27 23:28:03 +0100

    Tudo App Presentation(Flutter)...update 17

--- lib/main_screen/account_screen.dart
+++ lib/main_screen/account_screen.dart
@@ -7,7 +7,6 @@ class Account extends StatefulWidget {
   @override
   _AccountState createState() => _AccountState();
 }
-
 class _AccountState extends State<Account> {
   @override
   Widget build(BuildContext context) {
